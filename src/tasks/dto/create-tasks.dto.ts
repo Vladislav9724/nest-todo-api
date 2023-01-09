ef9855c01df6e6bfca185a5e97740a979642dacc
Vladislav9724 <vlad.tsyapka@gmail.com>
2023-01-09 19:37:05 +0200

    added validation in DTO

--- src/tasks/dto/create-tasks.dto.ts
+++ src/tasks/dto/create-tasks.dto.ts
@@ -1,5 +1,12 @@
+import { IsBoolean, IsString, Length, ValidatePromise } from "class-validator";
+
 export class CreateTasksDto{
+  @IsString({message:"Must be a string type"})
+  @Length(4, 15, {message:"Not less than 4 not more than 16"})
   readonly title: string
+  @IsString({message:"Must be a string type"})
+  @Length(2,7, {message:"Not less than 2 not more than 7"})
   readonly body: string
+  @IsBoolean({message:"Must be a boolean type"})
   readonly isCompleted?: boolean
 }
